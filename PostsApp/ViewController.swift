96b2f1fa011537ec8ddfb48d21f5da4f18cdd7f8
Rodrigo Rodovalho <rodrigomrodovalho@gmail.com>
2020-09-26 18:36:28 -0300

    Initial Commit

---
+++ PostsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostsApp
+//
+//  Created by Rodrigo Rodovalho on 26/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
